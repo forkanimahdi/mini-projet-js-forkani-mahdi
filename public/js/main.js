f545efa5b0cc0022e822ff546c0b48249d4ac3bd
forkanimahdi <forkanimahdi@gmail.com>
2023-06-08 17:21:07 +0100

    tittle of container

--- public/js/main.js
+++ public/js/main.js
@@ -22,12 +22,13 @@ advancedCreation.addEventListener("click", () => {
     let headerTitle = document.createElement("input")
     headerTitle.classList.add("header-input")
     headerTitle.setAttribute("type", "text")
+    headerTitle.setAttribute("maxlength", "15")
     headerTitle.setAttribute("value", "Task Tittle")
     headerTitle.readOnly = true
     containerHeader.appendChild(headerTitle)
     //* edit icon
     let editTitle = document.createElement("i")
-    editTitle.setAttribute("class", "fa-solid fa-pen")
+    editTitle.setAttribute("class", "fa-solid fa-pen edit-tittle")
     containerHeader.appendChild(editTitle)
     //* add task
     let addTask = document.createElement("i")
@@ -37,6 +38,25 @@ advancedCreation.addEventListener("click", () => {
     let changecolor = document.createElement("i")
     changecolor.setAttribute("class", "fa-sharp fa-solid fa-palette ms-3")
     containerHeader.appendChild(changecolor)
+
+    //* modification de titre
+
+    editTitle.addEventListener("click", () => {
+        if (editTitle.classList.contains("edit-tittle")) {
+            editTitle.removeAttribute("class")
+            editTitle.setAttribute("class","fa-solid fa-check")
+            headerTitle.readOnly = false
+            headerTitle.focus()
+        }else{
+            editTitle.removeAttribute("class")
+            editTitle.setAttribute("class", "fa-solid fa-pen edit-tittle")
+            headerTitle.readOnly = true
+            heade
+
+        }
+
+    })
+
 
 })
 
