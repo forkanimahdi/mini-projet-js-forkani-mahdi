c399aaa24129474b15ab7eb7d15c2ef6701bed7a
forkanimahdi <forkanimahdi@gmail.com>
2023-06-09 10:54:18 +0000

    creation d input

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,9 @@ let bodyAdvanced = document.querySelector(".task-column")
 let main = document.querySelector("main")
 let advancedCreation = document.querySelector(".button-creation")
 let containerHeader = document.querySelector(".task-column")
+// let allInsertTask = document.querySelectorAll(".header-task-area")
+
+
 
 let containerPosition = 1;
 
@@ -32,7 +35,7 @@ advancedCreation.addEventListener("click", () => {
     containerHeader.appendChild(editTitle)
     //* add task
     let addTask = document.createElement("i")
-    addTask.setAttribute("class", "fa-sharp fa-solid fa-plus ms-3")
+    addTask.setAttribute("class", "fa-sharp fa-solid fa-plus ms-3 type-task")
     containerHeader.appendChild(addTask)
     //* color palette
     let changecolor = document.createElement("i")
@@ -44,16 +47,53 @@ advancedCreation.addEventListener("click", () => {
     editTitle.addEventListener("click", () => {
         if (editTitle.classList.contains("edit-tittle")) {
             editTitle.removeAttribute("class")
-            editTitle.setAttribute("class","fa-solid fa-check")
+            editTitle.setAttribute("class", "fa-solid fa-check")
             headerTitle.readOnly = false
             headerTitle.focus()
-        }else{
+        } else {
             editTitle.removeAttribute("class")
             editTitle.setAttribute("class", "fa-solid fa-pen edit-tittle")
             headerTitle.readOnly = true
-            heade
+
 
         }
+
+    })
+
+    //* creation de taches
+
+    addTask.addEventListener('click', () => {
+
+        //*creation de task de l input
+        if (addTask.classList.contains("type-task")) {
+            let insertTask = document.createElement("input")
+            insertTask.classList.add("header-task-area")
+            insertTask.setAttribute("type", "text")
+            insertTask.setAttribute("maxlength", "40")
+            insertTask.setAttribute("placeholder", "Insert task here or press esc to cancel")
+            addTask.removeAttribute("class")
+            addTask.setAttribute("class", "fa-solid fa-check create-task")
+            addTask.style.transition = "0.5s "
+            editTitle.style.display = 'none'
+            changecolor.style.display = 'none'
+            headerTitle.style.display = 'none'
+            containerHeader.appendChild(insertTask)
+            insertTask.focus()
+            //* cancel task input using esc button
+            insertTask.addEventListener('keyup', (event) => {
+                if (event.key === "Escape") {
+                    containerHeader.removeChild(insertTask)
+                    addTask.removeAttribute("class")
+                    addTask.setAttribute("class", "fa-sharp fa-solid fa-plus ms-3 type-task")
+                    addTask.style.transition = "0s ease"
+                    editTitle.style.display = 'initial'
+                    changecolor.style.display = 'initial'
+                    headerTitle.style.display = 'initial'
+                }
+            })
+
+            //* creation de task
+        } 
 
     })
 
